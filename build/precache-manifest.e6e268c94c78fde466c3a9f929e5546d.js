2b052de1a027188819eb2987eb25822bba9d35ee
Jordan2551 <Jordan1558@gmail.com>
2020-06-18 17:48:56 -0400

    added meta title, description

---
+++ build/precache-manifest.e6e268c94c78fde466c3a9f929e5546d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa263d26741c255d9815d3e91bcb15bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3f98eb2bc6923f9106",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c1f8346e76c42f1aaaff",
+    "url": "/static/css/main.02ff23c1.chunk.css"
+  },
+  {
+    "revision": "7b3f98eb2bc6923f9106",
+    "url": "/static/js/2.df111da8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.df111da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f8346e76c42f1aaaff",
+    "url": "/static/js/main.1d8861cd.chunk.js"
+  },
+  {
+    "revision": "5b9079745ffca2bbefd8",
+    "url": "/static/js/runtime-main.1184d0cd.js"
+  },
+  {
+    "revision": "5096b1ad6e35cff85809702d388c0694",
+    "url": "/static/media/cards.5096b1ad.png"
+  },
+  {
+    "revision": "61b204985964a51f053af31b2252b2b4",
+    "url": "/static/media/hero-bg.61b20498.jpg"
+  },
+  {
+    "revision": "1ac3d7c13e2533be7be0925b0c25ab13",
+    "url": "/static/media/logo.1ac3d7c1.png"
+  },
+  {
+    "revision": "3aae38daf260a118423b511a89198e66",
+    "url": "/static/media/shield.3aae38da.png"
+  },
+  {
+    "revision": "22f68b96d555159930163b87369b66ea",
+    "url": "/static/media/stripe-logo.22f68b96.png"
+  }
+]);
